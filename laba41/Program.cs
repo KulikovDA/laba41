5ad90540175c8523a701367dd06369c1b038eaab
Arion <Arion@DESKTOP-9BU80RS>
2023-11-15 21:56:09 +0200

    Добавьте файлы проекта.

---
+++ laba41/Program.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int[] mas = new int[15];
+for (int i = 0; i < 15; i++)
+{
+    mas[i] = random.Next(-10, 11);
+    Console.WriteLine(mas[i]);
+}
+
+int negative = 0;
+int positive = 1;
+int zero = 0;
+
+for (int i = 0; i < 15; i++)
+{
+    if (mas[i] < 0)
+    {
+        negative++;
+    }
+    else if (mas[i] > 0)
+    {
+        positive *= mas[i];
+    }
+    else
+    {
+        zero++;
+    }
+}
+Console.WriteLine("Кол-во щтрицательных:"+negative);
+Console.WriteLine("Произведение положительных:"+positive);
+Console.WriteLine("Кол-во нулевых:"+zero);
